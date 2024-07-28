2fe73d4d62ce3ff48ef806d9d61812661eb8fc3e
李森淼 <lism@lisenmiaodeMBP2.lan>
2024-07-28 16:08:01 +0800

    add:log

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -210,6 +210,8 @@ export default defineConfig({
     nav: [
       { text: '主页', link: '/' },
       { text: '笔记', link: '/笔记/' },
+      { text: '视图', link: '/视图/' },
+
       { text: '最近更新', link: '/toc' },
     ],
     sidebar,
